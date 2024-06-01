b3009ef31b51784fe9948159d8453ac954dcf789
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-01 16:36:52 +0900

    Title: 장학금, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240601/장학금/scholarship.py
+++ 240601/장학금/scholarship.py
@@ -2,7 +2,7 @@ Midterm, Final = map(int,input().split())
 
 if Midterm < 90:
     print(0)
-elif Final > 95:
+elif Final >= 95:
     print(100000)
-elif Final > 90:
+elif Final >= 90:
     print(50000)
