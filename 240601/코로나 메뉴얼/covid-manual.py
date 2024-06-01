b6a70c1de50bcd2ddcf6d27826bb70bcfaf29e4f
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-01 17:37:52 +0900

    Title: 코로나 메뉴얼, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240601/코로나 메뉴얼/covid-manual.py
+++ 240601/코로나 메뉴얼/covid-manual.py
@@ -4,13 +4,13 @@ C_cold, C_temp = input().split()
 
 A_temp, B_temp, C_temp = int(A_temp), int(B_temp), int(C_temp)
 
-if (A_cold == 'Y' and A_temp >= 37) and (B_cold == 'Y' and B_temp >= 37):
-    print("E")
-elif (A_cold == 'Y' and A_temp >= 37) and (C_cold == 'Y' and C_temp >= 37):
-    print("E")
-elif (C_cold == 'Y' and C_temp >= 37) and (B_cold == 'Y' and B_temp >= 37):
-    print("E")
-elif (A_cold == 'Y' and A_temp >= 37) and (B_cold == 'Y' and B_temp >= 37) and (C_cold == 'Y' and C_temp >= 37):
-    print("E")
+if (A_cold == 'Y') and (A_temp >= 37):
+    if ((B_cold == 'Y') and (B_temp >= 37)) or ((C_cold == 'Y') and (C_temp >= 37)):
+        print("E")
+    else:
+        print("N")
 else:
-    print("N")
+    if ((B_cold == 'Y') and (B_temp >= 37)) and ((C_cold == 'Y') and (C_temp >= 37)):
+        print("E")
+    else:
+        print("N")
