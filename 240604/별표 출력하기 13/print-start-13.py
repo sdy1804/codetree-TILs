0b1aa1d1407dc540c9876b824fedd821aa458dcd
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-04 15:56:18 +0900

    Title: 별표 출력하기 13, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240604/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(n*2):
+    if i % 2 == 0:
+        for j in range(int(n-(i/2))):
+            print("*", end=" ")
+        print()
+    else:
+        for k in range(int((i+1)/2)):
+            print("*",end=" ")
+        print()
