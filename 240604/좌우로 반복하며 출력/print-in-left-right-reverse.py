0eabee6da7c99090517a5b307b8893f36c1b9b15
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-04 18:27:33 +0900

    Title: 좌우로 반복하며 출력, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240604/좌우로 반복하며 출력/print-in-left-right-reverse.py
+++ 240604/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -6,6 +6,6 @@ for i in range(n):
             print(j, end="")
         print()
     else:
-        for j in range(4, 0, -1):
+        for j in range(n, 0, -1):
             print(j, end="")
         print()
