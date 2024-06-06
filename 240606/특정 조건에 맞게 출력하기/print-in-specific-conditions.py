f042a25cb8e990970a50ecb07e726ad719a5eff9
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-06 16:23:47 +0900

    Title: 특정 조건에 맞게 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240606/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
@@ -0,0 +1,10 @@
+arr = list(map(int, input().split()))
+
+for i in range(len(arr)):
+    if arr[i] == 0:
+        arr = arr[:i]
+
+new_arr = [elem+3 if elem % 2 != 0 else elem//2 for elem in arr]
+
+for i in new_arr:
+    print(i, end=" ")
