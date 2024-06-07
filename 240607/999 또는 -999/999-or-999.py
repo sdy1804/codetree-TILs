32ee48290e411b01e5b84988a58b06ec348632e1
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-07 13:58:48 +0900

    Title: 999 또는 -999, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240607/999 또는 -999/999-or-999.py
@@ -0,0 +1,12 @@
+arr = list(map(int, input().split()))
+max_val = arr[0]
+min_val = arr[0]
+
+for elem in arr[1:]:
+    if elem == 999 or elem == -999:
+        break
+    if elem > max_val:
+        max_val = elem
+    if elem < min_val:
+        min_val = elem
+print(max_val, min_val)
