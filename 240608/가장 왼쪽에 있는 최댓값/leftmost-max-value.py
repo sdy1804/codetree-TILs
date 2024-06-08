f8c56737f6f560f1083aa994cdcabc2a2d5c97d9
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-08 13:31:18 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 203ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240608/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -0,0 +1,7 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+print(arr.index(max(arr)) + 1, end=" ")
+while arr.index(max(arr)) != 0:
+    arr = arr[:arr.index(max(arr))]
+    print(arr.index(max(arr)) + 1, end=" ")
