a4cb0a2cc02e8f2a4b99dfb8508dcd01afd3b733
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-09 22:04:25 +0900

    Title: 공백을 기준으로 출력, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240609/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,14 @@
+string_1 = input()
+string_2 = input()
+
+for i in string_1:
+    if i == " ":
+        print("", end="")
+    else:
+        print(i, end="")
+
+for i in string_2:
+    if i == " ":
+        print("", end="")
+    else:
+        print(i, end="")
