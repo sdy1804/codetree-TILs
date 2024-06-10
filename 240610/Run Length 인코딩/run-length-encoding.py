d6452a1f0e1bb8da88c28f1acd19c0f6ba5b4346
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-10 15:41:08 +0900

    Title: Run Length 인코딩, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240610/Run Length 인코딩/run-length-encoding.py
+++ 240610/Run Length 인코딩/run-length-encoding.py
@@ -7,12 +7,15 @@ for i in range(1, len(string)):
     if string[i] == string[i-1]:
         cnt += 1
         if i == len(string) - 1:
-            # print(f"{string[i-1]}{cnt}", end="")
-            new_string.append(f"{string[i-1]}{cnt}")    
+            new_string.append(f"{string[i]}{cnt}")
     else:
-        # print(f"{string[i-1]}{cnt}", end="")
-        new_string.append(f"{string[i-1]}{cnt}")
-        cnt = 1
+        if i == len(string) - 1:
+            new_string.append(f"{string[i-1]}{cnt}")
+            cnt = 1
+            new_string.append(f"{string[i]}{cnt}")
+        else:
+            new_string.append(f"{string[i-1]}{cnt}")
+            cnt = 1
 
 for i in range(len(new_string)):
     len_sum += len(new_string[i])
