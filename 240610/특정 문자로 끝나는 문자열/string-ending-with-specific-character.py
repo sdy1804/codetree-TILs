0a4831bc6a34406f29ed1dcc9cf1009e27137fdf
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-10 14:26:51 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240610/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
+++ 240610/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
@@ -1,8 +1,10 @@
 arr = [input() for _ in range(10)]
 alpha = input()
+exist = False
 
 for i in range(len(arr)):
     if arr[i][-1] == alpha:
         print(arr[i])
-    else:
-        print('None')
+        exist =True
+if exist == False:
+    print('None')
