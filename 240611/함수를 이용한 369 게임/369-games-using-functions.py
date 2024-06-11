c0667b0d2145d820edaa3740f6fc0f79f1ec8c3f
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-11 18:36:01 +0900

    Title: 함수를 이용한 369 게임, Time: 186ms, Memory: 25MB, Status: Passed - CodeTree

--- 240611/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240611/함수를 이용한 369 게임/369-games-using-functions.py
@@ -2,7 +2,8 @@ def is_three(n):
     return n % 3 == 0
 
 def is_three_six_nine(n):
-    return n % 10 == 3 or n % 10 == 6 or n % 10 == 9 or n // 10 == 3 or n // 10 == 6 or n // 10 == 9 or is_three(n) 
+    n = str(n)
+    return '3' in n or '6' in n or '9' in n or is_three(int(n)) 
 
 
 A, B = map(int, input().split())
