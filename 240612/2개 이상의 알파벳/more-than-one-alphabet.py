aabbf6d1f20471f09cd877899d5bfd3c73aef02d
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-12 18:29:24 +0900

    Title: 2개 이상의 알파벳, Time: 129ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240612/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -0,0 +1,12 @@
+def check_alpha(string):
+    cnt = 0
+    for i in range(len(string)-1):
+        if string[i] != string[i+1]:
+            cnt += 1
+    return cnt
+
+A = input()
+if check_alpha(A) >= 2:
+    print('Yes')
+else:
+    print('No')
