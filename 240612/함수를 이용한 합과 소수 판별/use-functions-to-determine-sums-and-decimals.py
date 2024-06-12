e5b8a14bf9a4124014a6cfeb23eea9a01ae8dca4
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-12 16:19:20 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240612/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
+++ 240612/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -1,7 +1,11 @@
 def is_even(n):
-    n = str(n)
-    if (int(n[0]) + int(n[1])) % 2 == 0:
-        return True
+    if n < 10:
+        if n % 2 == 0:
+            return True
+    else:
+        n = str(n)
+        if (int(n[0]) + int(n[1])) % 2 == 0:
+            return True
 
 def is_prime(n):
     for j in range(2, i):
