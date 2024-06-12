f018dae5f3b0a7d601890c30e0a3015ff85098cd
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-12 16:54:30 +0900

    Title: 2021년 날짜의 유무, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240612/2021년 날짜의 유무/with-or-without-2021.py
+++ 240612/2021년 날짜의 유무/with-or-without-2021.py
@@ -2,7 +2,9 @@ def is_days(m, d):
     if m >= 1 and m <= 12:
         if m == 2 and d >= 1 and d <= 28:
             return True
-        elif m < 8 and m % 2 != 0:
+        else:
+            return False
+        if m < 8 and m % 2 != 0:
             if d >= 1 and d <= 31:
                 return True
         elif m < 8 and m % 2 == 0:
