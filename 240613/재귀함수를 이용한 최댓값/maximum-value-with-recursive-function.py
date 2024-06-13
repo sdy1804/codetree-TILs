d88d0a92c8e165f0f99fbe946428af60c1404d39
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-13 17:08:13 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240613/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240613/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -1,16 +1,18 @@
 N = int(input())
 array = list(map(int, input().split()))
 
-def max_val(n, arr):
-    max_value = 0
+def max_val(n):
+    # print(f"max_val{n} is called")
+    arr = array[:n]
     if n == 1:
-        return arr[n-1]
-    if max_val(n-1, arr[:n-1]) < arr[n-1]:
-        max_value = arr[n-1]
+        return arr[0]
+
+    max_value = max_val(n-1)
+    if max_value > arr[n-1]:
         return max_value
     else:
-        max_value = max_val(n-1, arr[:n-1])
+        max_value = arr[n-1]
         return max_value
         
 
-print(max_val(N, array))
+print(max_val(N))
