5ca01de4530fb2a06f1a351b8ece49d269903d0f
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-14 16:59:47 +0900

    Title: k번째로 신기한 문자열, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240614/k번째로 신기한 문자열/kth-special-string.py
+++ 240614/k번째로 신기한 문자열/kth-special-string.py
@@ -3,13 +3,17 @@ n, k = int(n), int(k)
 arr = [input() for _ in range(n)]
 
 T_arr = []
+# for i in range(n):
+#     cnt = 0
+#     for j in range(len(T)):
+#         if arr[i][j] == T[j]:
+#             cnt += 1
+#     if cnt == len(T):
+#         T_arr.append(arr[i])
 for i in range(n):
-    cnt = 0
-    for j in range(len(T)):
-        if arr[i][j] == T[j]:
-            cnt += 1
-    if cnt == len(T):
-        T_arr.append(arr[i])
+    if len(arr[i]) > len(T):
+        if arr[i][:len(T)] == T:
+            T_arr.append(arr[i])
 
 T_arr.sort()
 
