48949f690934ae19122c48c7b67aeca4991019e5
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-14 16:52:08 +0900

    Title: k번째로 신기한 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240614/k번째로 신기한 문자열/kth-special-string.py
@@ -0,0 +1,16 @@
+n, k, T = input().split()
+n, k = int(n), int(k)
+arr = [input() for _ in range(n)]
+
+T_arr = []
+for i in range(n):
+    cnt = 0
+    for j in range(len(T)):
+        if arr[i][j] == T[j]:
+            cnt += 1
+    if cnt == len(T):
+        T_arr.append(arr[i])
+
+T_arr.sort()
+
+print(T_arr[k-1])
