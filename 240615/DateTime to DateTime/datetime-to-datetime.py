1c2e9908f82b5dbee752a0b9384b1574a900071c
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-15 14:43:22 +0900

    Title: DateTime to DateTime, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240615/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,20 @@
+a, b, c = map(int, input().split())
+
+def count_mins(d, h, m):
+    day = 0
+    hour, mins, elapsed_time = 0, 0, 0
+    while 1:
+        if hour == h and mins == m:
+            break
+        if mins == 60:
+            hour += 1
+            mins = 0
+        mins += 1
+        elapsed_time += 1
+    elapsed_time += d * 60 * 24
+    return elapsed_time
+
+if (count_mins(a, b, c) - count_mins(11, 11, 11)) > 0:
+    print(count_mins(a, b, c) - count_mins(11, 11, 11))
+else:
+    print(-1)
