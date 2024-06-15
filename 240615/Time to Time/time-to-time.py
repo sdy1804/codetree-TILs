af794db78ab6266ea74705bf06e5c344e763f634
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-15 14:05:59 +0900

    Title: Time to Time, Time: 154ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240615/Time to Time/time-to-time.py
@@ -0,0 +1,24 @@
+a, b, c, d = map(int, input().split())
+first_elapsed_time = 0
+second_elapsed_time = 0
+min, hour = 0, 0
+
+while 1:
+    if hour == a and min == b:
+        break
+    if min == 60:
+        hour += 1
+        min = 0 
+    min += 1
+    first_elapsed_time += 1
+# print(first_elapsed_time)
+
+while 1:
+    if hour == c and min == d:
+        break
+    if min == 60:
+        hour += 1
+        min = 0 
+    min += 1
+    second_elapsed_time += 1
+print(second_elapsed_time)
