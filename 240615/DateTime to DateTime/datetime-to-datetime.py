12246b491d2b65a30481905f4e7544dd2d706276
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-15 14:45:48 +0900

    Title: DateTime to DateTime, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240615/DateTime to DateTime/datetime-to-datetime.py
+++ 240615/DateTime to DateTime/datetime-to-datetime.py
@@ -1,20 +1,26 @@
 a, b, c = map(int, input().split())
 
-def count_mins(d, h, m):
-    day = 0
-    hour, mins, elapsed_time = 0, 0, 0
-    while 1:
-        if hour == h and mins == m:
-            break
-        if mins == 60:
-            hour += 1
-            mins = 0
-        mins += 1
-        elapsed_time += 1
-    elapsed_time += d * 60 * 24
-    return elapsed_time
+# def count_mins(d, h, m):
+#     day = 0
+#     hour, mins, elapsed_time = 0, 0, 0
+#     while 1:
+#         if hour == h and mins == m:
+#             break
+#         if mins == 60:
+#             hour += 1
+#             mins = 0
+#         mins += 1
+#         elapsed_time += 1
+#     elapsed_time += d * 60 * 24
+#     return elapsed_time
 
-if (count_mins(a, b, c) - count_mins(11, 11, 11)) >= 0:
-    print(count_mins(a, b, c) - count_mins(11, 11, 11))
+# if (count_mins(a, b, c) - count_mins(11, 11, 11)) >= 0:
+#     print(count_mins(a, b, c) - count_mins(11, 11, 11))
+# else:
+#     print(-1)
+diff = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11)
+
+if diff >= 0:
+    print(diff)
 else:
     print(-1)
