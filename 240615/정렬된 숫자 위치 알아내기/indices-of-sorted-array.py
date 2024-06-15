713e99672a5c800fae416144869e92be7b910ecf
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-15 13:48:59 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240615/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
+++ 240615/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -5,6 +5,7 @@ class Seq:
         self.aftPos = aftPos
 
 # 체크해야 할 점 : 인스턴스들을 리스트1에 저장한 후, 리스트2로 소팅을 하면 과연 리스트2에 인스턴스 내용들이 그대로 있는가? -> Yes
+# 객체 참조
 
 N = int(input())
 arr = list(map(int, input().split()))
@@ -21,7 +22,10 @@ for index in range(len(arr)):
     sorted_info[index].aftPos = index + 1
     # print(f'{sorted_info[index].val}, {sorted_info[index].befPos}', sorted_info[index].aftPos)
 
-for i in range(1, N+1):
-    for j in range(N):
-        if i == sorted_info[j].befPos:
-            print(sorted_info[j].aftPos, end=" ")
+for index in range(N):
+    print(info[index].aftPos, end=" ") # sorted_info에서 참조하는 객체들이 info에서도 참조되는 것을 이용.
+
+# for i in range(1, N+1):
+#     for j in range(N):
+#         if i == sorted_info[j].befPos:
+#             print(sorted_info[j].aftPos, end=" ")
