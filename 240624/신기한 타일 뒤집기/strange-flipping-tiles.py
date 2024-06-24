8bf7b7af6e113db2926745369180fe0ee1b6df9a
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-24 18:47:12 +0900

    Title: 신기한 타일 뒤집기, Time: 187ms, Memory: 27MB, Status: Passed - CodeTree

--- 240624/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240624/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -3,10 +3,10 @@ arr = list(input().split() for _ in range(n))
 for i in range(len(arr)):
     arr[i][0] = int(arr[i][0])
 
-ans = [0] * 20
+ans = [0] * 100 * 1000 * 2
 
 # 검은색 = 1, 흰색 = 2
-start_point = 10
+start_point = 100000
 for i in range(len(arr)):
     if arr[i][1] == 'R':
         for j in range(start_point, start_point + arr[i][0]):
