a788f7806bcad8b3d6f9f52f897472ac9707eec3
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-24 18:46:27 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240624/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = list(input().split() for _ in range(n))
+for i in range(len(arr)):
+    arr[i][0] = int(arr[i][0])
+
+ans = [0] * 20
+
+# 검은색 = 1, 흰색 = 2
+start_point = 10
+for i in range(len(arr)):
+    if arr[i][1] == 'R':
+        for j in range(start_point, start_point + arr[i][0]):
+            ans[j] = 1
+        start_point = start_point + arr[i][0] - 1
+    else:
+        for j in range(start_point, start_point - arr[i][0], -1):
+            ans[j] = 2
+        start_point = start_point - arr[i][0] + 1
+print(ans.count(2), ans.count(1))
