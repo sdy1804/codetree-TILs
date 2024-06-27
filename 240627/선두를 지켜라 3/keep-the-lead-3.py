b7e11abbabd69900183ec3c6a7f77c0a2fd9d4e3
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-06-27 19:01:07 +0900

    Title: 선두를 지켜라 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240627/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,37 @@
+N, M = map(int, input().split())
+move_A = [list(map(int, input().split())) for _ in range(N)]
+move_B = [list(map(int, input().split())) for _ in range(M)]
+
+length_arr = 20
+arr_A = [0] * length_arr
+arr_B = [0] * length_arr
+
+A_now_index = 1
+for i in range(len(move_A)):
+    for _ in range(move_A[i][1]):
+        arr_A[A_now_index] = arr_A[A_now_index - 1] + move_A[i][0]
+        A_now_index += 1
+for i in range(len(arr_A)):
+    if i >= A_now_index:
+        arr_A[i] = arr_A[A_now_index-1]
+# print(arr_A)
+
+B_now_index = 1
+for i in range(len(move_B)):
+    for _ in range(move_B[i][1]):
+        arr_B[B_now_index] = arr_B[B_now_index - 1] + move_B[i][0]
+        B_now_index += 1
+for i in range(len(arr_B)):
+    if i >= B_now_index:
+        arr_B[i] = arr_B[B_now_index-1]
+# print(arr_B)
+
+case = 0
+for i in range(1, len(arr_A)):
+    if arr_A[i] > arr_B[i] and arr_A[i-1] <= arr_B[i-1]:
+        case += 1
+    elif arr_A[i] < arr_B[i] and arr_A[i-1] >= arr_B[i-1]:
+        case += 1
+    elif arr_A[i] == arr_B[i] and (arr_A[i-1] < arr_B[i-1] or arr_A[i-1] > arr_B[i-1]):
+        case += 1
+print(case)
