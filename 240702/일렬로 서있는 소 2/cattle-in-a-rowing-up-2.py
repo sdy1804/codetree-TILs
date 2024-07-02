a8a79bd21d14ae250f88584d8b828d47aa08b713
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-02 17:21:58 +0900

    Title: 일렬로 서있는 소 2, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240702/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240702/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -6,11 +6,11 @@ for i in range(N):
     first = arr[i]
     # print('first', first)
     for j in range(i+1, N):
-        if arr[j] > first:
+        if arr[j] >= first:
             second = arr[j]
             # print('second', second)
             for k in range(j+1, N):
-                if arr[k] > second:
+                if arr[k] >= second:
                     cnt += 1
                     # print(cnt)
 print(cnt)
