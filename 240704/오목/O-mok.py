356ab59708ce653f56ad80eb90c610f4f9bf0e02
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-04 17:40:42 +0900

    Title: 오목, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/오목/O-mok.py
+++ 240704/오목/O-mok.py
@@ -1,36 +1,44 @@
 arr = [list(map(int,input().split())) for _ in range(19)]
 
 # 가로, 세로, 대각선으로 1 또는 2가 5개로 나오는지 체크
+# 벗어나는지 체크
+
+def in_range(x, y):
+    return x >= 0 and x < len(arr) and y >= 0 and y < len(arr)
 
 win_state = 0
 for i in range(len(arr)):
     for j in range(len(arr[i])):
-        if arr[i][j] == 1 and arr[i][j+1] == 1 and arr[i][j+2] == 1 and arr[i][j+3] == 1 and arr[i][j+4] == 1:
-            win_state = 1
-            center_x, center_y = i+1, j+2+1
-        elif arr[i][j] == 2 and arr[i][j+1] == 2 and arr[i][j+2] == 2 and arr[i][j+3] == 2 and arr[i][j+4] == 2:
-            win_state = 2
-            center_x, center_y = i+1, j+2+1
-
-        if arr[i][j] == 1 and arr[i+1][j] == 1 and arr[i+2][j] == 1 and arr[i+3][j] == 1 and arr[i+4][j] == 1:
-            win_state = 1
-            center_x, center_y = i+2+1, j+1
-        elif arr[i][j] == 2 and arr[i+1][j] == 2 and arr[i+2][j] == 2 and arr[i+3][j] == 2 and arr[i+4][j] == 2:
-            win_state = 2
-            center_x, center_y = i+2+1, j+1
-        
-        if arr[i][j] == 1 and arr[i+1][j+1] == 1 and arr[i+2][j+2] == 1 and arr[i+3][j+3] == 1 and arr[i+4][j+4] == 1:
-            win_state = 1
-            center_x, center_y = i+2+1, j+2+1
-        elif arr[i][j] == 2 and arr[i+1][j+1] == 2 and arr[i+2][j+2] == 2 and arr[i+3][j+3] == 2 and arr[i+4][j+4] == 2:
-            win_state = 2
-            center_x, center_y = i+2+1, j+2+1
-        elif arr[i][j] == 1 and arr[i+1][j-1] == 1 and arr[i+2][j-2] == 1 and arr[i+3][j-3] == 1 and arr[i+4][j-4] == 1:
-            win_state = 1
-            center_x, center_y = i+2+1, j-2+1
-        elif arr[i][j] == 2 and arr[i+1][j-1] == 2 and arr[i+2][j-2] == 2 and arr[i+3][j-3] == 2 and arr[i+4][j-4] == 2:
-            win_state = 2
-            center_x, center_y = i+2+1, j-2+1
+        # 가로
+        if in_range(i, j+1) and in_range(i, j+2) and in_range(i, j+2) and in_range(i, j+2):
+            if arr[i][j] == 1 and arr[i][j+1] == 1 and arr[i][j+2] == 1 and arr[i][j+3] == 1 and arr[i][j+4] == 1:
+                win_state = 1
+                center_x, center_y = i+1, j+2+1
+            elif arr[i][j] == 2 and arr[i][j+1] == 2 and arr[i][j+2] == 2 and arr[i][j+3] == 2 and arr[i][j+4] == 2:
+                win_state = 2
+                center_x, center_y = i+1, j+2+1
+        # 세로
+        if in_range(i+1, j) and in_range(i+2, j) and in_range(i+3, j) and in_range(i+4, j):
+            if arr[i][j] == 1 and arr[i+1][j] == 1 and arr[i+2][j] == 1 and arr[i+3][j] == 1 and arr[i+4][j] == 1:
+                win_state = 1
+                center_x, center_y = i+2+1, j+1
+            elif arr[i][j] == 2 and arr[i+1][j] == 2 and arr[i+2][j] == 2 and arr[i+3][j] == 2 and arr[i+4][j] == 2:
+                win_state = 2
+                center_x, center_y = i+2+1, j+1
+        # 대각선
+        if in_range(i+1, j+1) and in_range(i+2, j+2) and in_range(i+3, j+3) and in_range(i+4, j+4) and in_range(i+1, j-1) and in_range(i+2, j-2) and in_range(i+3, j-3) and in_range(i+4, j-4):
+            if arr[i][j] == 1 and arr[i+1][j+1] == 1 and arr[i+2][j+2] == 1 and arr[i+3][j+3] == 1 and arr[i+4][j+4] == 1:
+                win_state = 1
+                center_x, center_y = i+2+1, j+2+1
+            elif arr[i][j] == 2 and arr[i+1][j+1] == 2 and arr[i+2][j+2] == 2 and arr[i+3][j+3] == 2 and arr[i+4][j+4] == 2:
+                win_state = 2
+                center_x, center_y = i+2+1, j+2+1
+            elif arr[i][j] == 1 and arr[i+1][j-1] == 1 and arr[i+2][j-2] == 1 and arr[i+3][j-3] == 1 and arr[i+4][j-4] == 1:
+                win_state = 1
+                center_x, center_y = i+2+1, j-2+1
+            elif arr[i][j] == 2 and arr[i+1][j-1] == 2 and arr[i+2][j-2] == 2 and arr[i+3][j-3] == 2 and arr[i+4][j-4] == 2:
+                win_state = 2
+                center_x, center_y = i+2+1, j-2+1
 
 if win_state == 0:
     print(win_state)
