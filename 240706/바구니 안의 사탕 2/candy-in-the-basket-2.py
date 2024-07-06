9c5f81683389c2337c190197ca6065a3fd0af780
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-06 18:11:46 +0900

    Title: 바구니 안의 사탕 2, Time: 152ms, Memory: 25MB, Status: Passed - CodeTree

--- 240706/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240706/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -3,17 +3,27 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 
 # 리스트 만들고 위치에 해당하는 사탕 갯수 추가
 # 구간 설정하여 완전 탐색
+# 0 ~ 15까지 만들었을 때, K = 3
 # c가 0일 경우 -> -3, 3
 # c가 3일 경우 -> 0, 6
+# c = 4 -> 1, 7
+# c = 10 -> 7, 13
+# c = 12 -> 9, 15
+# c = 13 -> 10, 16
+
+# 0~100까지 만들었을 때, K = 72
+# c = 0 -> -72, 72
+# c = 100-72 = 28 -> 
+# c = 72 -> 0, 72+72=144
 # out of range가 되는 경우 유의
-length = 101
+length = 10000
 candy_list = [0] * length
 for i in range(len(arr)):
     candy_list[arr[i][1]] += arr[i][0]
 # print(candy_list)
 
 total_max = 0
-for i in range(K, len(candy_list) - K): # c 범위
+for i in range(K, len(candy_list) - K): # 
     # print(i)
     # print('new')
     now_max = 0
