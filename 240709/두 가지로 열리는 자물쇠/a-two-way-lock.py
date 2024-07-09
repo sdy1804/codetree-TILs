8b45de13903bbddabc0fff28889827b599c17ce7
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-09 17:07:28 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 299ms, Memory: 29MB, Status: Passed - CodeTree

--- 240709/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240709/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -7,9 +7,9 @@ first = []
 for i in range(1, N + 1):
     for j in range(1, N + 1):
         for k in range(1, N + 1):
-            if ((abs(i - arr1[0]) <= 2) or (arr1[0] == 1 and i == N) or (arr1[0] == 1 and i == N-1) or (arr1[0] == 2 and i == N))\
-            and ((abs(j - arr1[1]) <= 2) or (arr1[1] == 1 and j == N) or (arr1[1] == 1 and j == N-1) or (arr1[1] == 2 and j == N)) \
-            and ((abs(k - arr1[2]) <= 2) or (arr1[2] == 1 and k == N) or (arr1[2] == 1 and k == N-1) or (arr1[2] == 2 and k == N)):
+            if ((abs(i - arr1[0]) <= 2) or (arr1[0] == N and i == 1) or (arr1[0] == N and i == 2) or (arr1[0] == N - 1 and i == 1) or (arr1[0] == 1 and i == N) or (arr1[0] == 1 and i == N-1) or (arr1[0] == 2 and i == N))\
+            and ((abs(j - arr1[1]) <= 2) or (arr1[1] == N and j == 1) or (arr1[1] == N and j == 2) or (arr1[1] == N - 1 and j == 1) or (arr1[1] == 1 and j == N) or (arr1[1] == 1 and j == N-1) or (arr1[1] == 2 and j == N)) \
+            and ((abs(k - arr1[2]) <= 2) or (arr1[2] == N and k == 1) or (arr1[2] == N and k == 2) or (arr1[2] == N - 1 and k == 1) or (arr1[2] == 1 and k == N) or (arr1[2] == 1 and k == N-1) or (arr1[2] == 2 and k == N)):
                 # print('i',i, j, k)
                 first.append([i, j, k])
                 cnt += 1
@@ -20,9 +20,9 @@ second = []
 for i in range(1, N + 1):
     for j in range(1, N + 1):
         for k in range(1, N + 1):
-            if ((abs(i - arr2[0]) <= 2) or (arr2[0] == 1 and i == N) or (arr2[0] == 1 and i == N-1) or (arr2[0] == 2 and i == N-1))\
-            and ((abs(j - arr2[1]) <= 2) or (arr2[1] == 1 and j == N) or (arr2[1] == 1 and j == N-1) or (arr2[1] == 2 and j == N-1)) \
-            and ((abs(k - arr2[2]) <= 2) or (arr2[2] == 1 and k == N) or (arr2[2] == 1 and k == N-1) or (arr2[2] == 2 and k == N-1)):
+            if ((abs(i - arr2[0]) <= 2) or (arr2[0] == N and i == 1) or (arr2[0] == N and i == 2) or (arr2[0] == N - 1 and i == 1) or (arr2[0] == 1 and i == N) or (arr2[0] == 1 and i == N-1) or (arr2[0] == 2 and i == N-1))\
+            and ((abs(j - arr2[1]) <= 2) or (arr2[1] == N and j == 1) or (arr2[1] == N and j == 2) or (arr2[1] == N - 1 and j == 1) or (arr2[1] == 1 and j == N) or (arr2[1] == 1 and j == N-1) or (arr2[1] == 2 and j == N-1)) \
+            and ((abs(k - arr2[2]) <= 2) or (arr2[2] == N and k == 1) or (arr2[2] == N and k == 2) or (arr2[2] == N - 1 and k == 1) or (arr2[2] == 1 and k == N) or (arr2[2] == 1 and k == N-1) or (arr2[2] == 2 and k == N-1)):
                 # print('i',i, j, k)
                 second.append([i, j, k])
                 cnt += 1
