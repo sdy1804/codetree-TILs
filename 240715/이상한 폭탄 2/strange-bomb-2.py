57c8fdba2aa77d8be94c48769cc478c179f23870
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-15 18:33:55 +0900

    Title: 이상한 폭탄 2, Time: 234ms, Memory: 25MB, Status: Passed - CodeTree

--- 240715/이상한 폭탄 2/strange-bomb-2.py
+++ 240715/이상한 폭탄 2/strange-bomb-2.py
@@ -1,16 +1,17 @@
 N, K = map(int, input().split())
 arr = [int(input()) for _ in range(N)]
 
-# 완탐으로 K까지의 쌍을 만듦
-# 만약 두 쌍이 같다면 max값으로 저장
+# 완탐으로 쌍을 만듦
+# 만약 두 쌍이 같고, K범위 안이면 max값으로 저장
 # 새롭게 같은 두쌍이 발생하면 max값 갱신
-total_max = 0
-for i in range(N - K):
-    for j in range(i + 1, i + K + 1):
+total_max = -100
+for i in range(len(arr)):
+    for j in range(i + 1, len(arr)):
         now_max = 0
-        # print(i, j)
-        if arr[i] == arr[j]:
+        if arr[i] == arr[j] and j - i <= K:
             now_max = arr[i]
+        else:
+            now_max = -1
         # print(now_max)
         total_max = max(total_max, now_max)
 print(total_max)
