32c3a84a05123bf9c49581184fc27ec54bb4f722
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-16 14:48:01 +0900

    Title: 스승의 은혜 3, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240716/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,23 @@
+N, B = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+arr.sort(key=lambda x: x[0])
+
+total_cnt = 0
+for i in range(len(arr)):
+    total_sum = 0
+    now_cnt = 0
+    for j in range(len(arr)):
+        if i == j:
+            arr[j][0] = arr[j][0] // 2
+        # print(arr[j])
+        total_sum += sum(arr[j])
+        # print('total_sum', total_sum)
+        if total_sum <= B:
+            now_cnt += 1
+            # print('cnt', now_cnt)
+        total_cnt = max(now_cnt, total_cnt)
+        # print('total_cnt', total_cnt)
+        if i == j:
+            arr[j][0] = arr[j][0] * 2
+print(total_cnt)
