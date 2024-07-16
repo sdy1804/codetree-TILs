0e9ed4f3d0b46914c63e4706c0a6a51bad7f9be3
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-16 15:15:45 +0900

    Title: 스승의 은혜 3, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240716/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240716/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,7 +1,8 @@
 N, B = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-arr.sort(key=lambda x: x[0])
+arr.sort(key=lambda x: (x[0] + x[1]))
+# print(arr)
 
 total_cnt = 0
 for i in range(len(arr)):
@@ -10,14 +11,14 @@ for i in range(len(arr)):
     for j in range(len(arr)):
         if i == j:
             arr[j][0] = arr[j][0] // 2
-        # print(arr[j])
+        print(arr[j])
         total_sum += sum(arr[j])
-        # print('total_sum', total_sum)
+        print('total_sum', total_sum)
         if total_sum <= B:
             now_cnt += 1
-            # print('cnt', now_cnt)
+            print('cnt', now_cnt)
         total_cnt = max(now_cnt, total_cnt)
-        # print('total_cnt', total_cnt)
+        print('total_cnt', total_cnt)
         if i == j:
             arr[j][0] = arr[j][0] * 2
 print(total_cnt)
