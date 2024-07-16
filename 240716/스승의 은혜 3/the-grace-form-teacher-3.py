7a2edb196ca88c61b73232f422106410b22e3929
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-16 16:51:25 +0900

    Title: 스승의 은혜 3, Time: 2308ms, Memory: 27MB, Status: Passed - CodeTree

--- 240716/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240716/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,8 +1,13 @@
 N, B = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 arr2 = arr.copy()
+arr3 = arr.copy()
 arr.sort(key=lambda x: (x[0] + x[1]))
+# print(arr)
 arr2.sort(key=lambda x: (x[0] // 2 + x[1]))
+# print(arr2)
+arr3.sort(key=lambda x: (x[0] + x[1], x[0] // 2 + x[1]))
+# print(arr3)
 # print(arr)
 
 total_cnt = 0
@@ -12,7 +17,7 @@ for i in range(len(arr)):
     for j in range(len(arr)):
         if i == j:
             arr[j][0] = arr[j][0] // 2
-        # print(arr[j])
+        # print(arr[j], sum(arr[j]), arr[j][0] // 2 + arr[j][1])
         total_sum += sum(arr[j])
         # print('total_sum', total_sum)
         if total_sum <= B:
@@ -42,4 +47,28 @@ for i in range(len(arr2)):
         if i == j:
             arr2[j][0] = arr2[j][0] * 2
 # print(total_cnt2)
-print(max(total_cnt2, total_cnt))
+
+total_cnt3 = 0
+for i in range(len(arr3)):
+    total_sum3 = 0
+    now_cnt3 = 0
+    for j in range(len(arr3)):
+        # print(arr[j])
+        total_sum3 += sum(arr3[j])
+        # print('total_sum', total_sum)
+        if total_sum3 <= B:
+            now_cnt3 += 1
+        else:
+            total_sum3 -= sum(arr3[j])
+            for k in range(j, len(arr3)):
+                # print(arr3[k], total_sum3)
+                # print(arr3[k][0] // 2 + arr3[k][1] + total_sum3)
+                if (arr3[k][0] // 2 + arr3[k][1]) + total_sum3 <= B:
+                    now_cnt3 += 1
+                    total_sum3 += arr3[k][0] // 2 + arr3[k][1]
+        total_cnt3 = max(now_cnt3, total_cnt3)
+        # print('total_cnt', total_cnt)
+        if i == j:
+            arr3[j][0] = arr3[j][0] * 2
+# print(total_cnt3)
+print(max(total_cnt, total_cnt2, total_cnt3))
