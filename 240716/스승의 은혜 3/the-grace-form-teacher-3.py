d76473ea6a606df448c1472259592dc749c238fb
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-16 15:18:51 +0900

    Title: 스승의 은혜 3, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240716/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240716/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,7 +1,8 @@
 N, B = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-
+arr2 = arr.copy()
 arr.sort(key=lambda x: (x[0] + x[1]))
+arr2.sort(key=lambda x: (x[0] // 2 + x[1]))
 # print(arr)
 
 total_cnt = 0
@@ -21,4 +22,24 @@ for i in range(len(arr)):
         # print('total_cnt', total_cnt)
         if i == j:
             arr[j][0] = arr[j][0] * 2
-print(total_cnt)
+# print(total_cnt)
+
+total_cnt2 = 0
+for i in range(len(arr2)):
+    total_sum2 = 0
+    now_cnt2 = 0
+    for j in range(len(arr2)):
+        if i == j:
+            arr2[j][0] = arr2[j][0] // 2
+        # print(arr[j])
+        total_sum2 += sum(arr2[j])
+        # print('total_sum', total_sum)
+        if total_sum2 <= B:
+            now_cnt2 += 1
+            # print('cnt', now_cnt)
+        total_cnt2 = max(now_cnt2, total_cnt2)
+        # print('total_cnt', total_cnt)
+        if i == j:
+            arr2[j][0] = arr2[j][0] * 2
+# print(total_cnt2)
+print(max(total_cnt2, total_cnt))
