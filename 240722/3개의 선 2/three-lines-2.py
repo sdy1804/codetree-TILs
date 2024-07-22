7f2679429f57d8298013eab36e208bcf964c1aef
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-22 16:29:57 +0900

    Title: 3개의 선 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240722/3개의 선 2/three-lines-2.py
+++ 240722/3개의 선 2/three-lines-2.py
@@ -7,52 +7,48 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 # 다시한번 돌면서 남은 좌표들에서 x, y중 최대로 많은 위치로 설정했을 때
 # 모든 좌표가 사라지는지 체크
 
-total_max_x = 0
+total_max = 0
 x_point = 0
-for i in range(11):
-    now_max_x = 0
-    for j in range(len(arr)):
-        if arr[j][0] == i:
-            now_max_x += 1
-    total_max_x = max(now_max_x, total_max_x)
-    if total_max_x == now_max_x:
-        x_point = i
-# print(total_max_x, x_point)
-
+y_point = 0
 arr2 = arr.copy()
 
-for i in range(len(arr)):
-    if arr[i][0] == x_point:
-        arr2.remove(arr[i])
-# print(arr2)
+for i in range(2):
+    x_max, y_max = False, False
+    for j in range(11):
+        now_max_x = 0
+        now_max_y = 0
+        for k in range(len(arr)):
+            if arr[k][0] == i:
+                now_max_x += 1
+            if arr[k][1] == i:
+                now_max_y += 1
+        # print('i, now_max_x, now_max_y', i, now_max_x, now_max_y)
+        total_max = max(now_max_x, now_max_y, total_max)
+        if total_max == now_max_x:
+            x_point = i
+            x_max = True
+        elif total_max == now_max_y:
+            y_point = i
+            y_max = True
+    rmv_list = []
+    for l in range(len(arr2)):
+        if x_max == True:
+            if arr2[l][0] == x_point:
+                rmv_list.append(arr2[l])
+        elif y_max == True:
+            if arr2[l][1] == y_point:
+                rmv_list.append(arr2[l])
+    # print('rmv_list', rmv_list)
+    for l in range(len(rmv_list)):
+        if rmv_list[l] in arr2:
+            arr2.remove(rmv_list[l])
+    # print(arr2)
 
-total_max_y = 0
-y_point = 0
-for i in range(11):
-    now_max_y = 0
-    for j in range(len(arr2)):
-        if arr2[j][1] == i:
-            now_max_y += 1
-    total_max_y = max(now_max_y, total_max_y)
-    if total_max_y == now_max_y:
-        y_point = i
-# print(total_max_y, y_point)
-
-for i in range(len(arr)):
-    if arr[i][1] == y_point:
-        arr2.remove(arr[i])
-# print(arr2)
-
-all_x, all_y = True, True
+all_same = False
 for i in range(len(arr2)):
-    temp_x = arr2[0][0]
-    temp_y = arr2[0][1]
-    # print(temp_x, temp_y)
-    if arr2[i][0] != temp_x:
-        all_x = False
-    if arr2[i][0] != temp_y:
-        all_y = False
-if all_x == False and all_y == False:
-    print(0)
-else:
+    if len(arr2) == 1 or arr[i][0] == arr[0][0] or arr[i][1] == arr[0][1]:
+        all_same = True
+if all_same == True:
     print(1)
+else:
+    print(0)
