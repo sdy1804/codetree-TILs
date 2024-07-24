f49e9f0aebab58ce2a8679bb54ca426eeb715034
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-24 17:33:13 +0900

    Title: 숫자들의 최대 차, Time: 5090ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 240724/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 240724/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -4,11 +4,12 @@ arr = [int(input()) for _ in range(N)]
 # 1. 만들 수 있는 리스트의 조합을 완탐해서 min, max?
 # 2. 최대 최소값을 하나씩 찝은후, 최대 최소와의 값 차이가 K 이하면 나머지 숫자들을 탐색
 # 탐색하면서 최대 최소값을 제외한 나머지가 차이를 구했을 때 K 이하이면 append
-
+total_max_len = 0
 for i in range(N):
     for j in range(i+1, N):
         temp_list = []
-        if abs(arr[i] - arr[j]) <= 3:
+        now_max_len = 0
+        if abs(arr[i] - arr[j]) <= K:
             now_min = min(arr[i], arr[j])
             now_max = max(arr[i], arr[j])
             temp_list.append(now_min)
@@ -16,8 +17,13 @@ for i in range(N):
             for k in range(N):
                 # print(arr[i], arr[j])
                 if (k == i or k == j):
+                    # print('aa',temp_list)
                     continue
-                if abs(arr[k] - now_min) <= 3 and abs(arr[k] - now_max) <= 3:
+                if abs(arr[k] - now_min) <= K and abs(arr[k] - now_max) <= K:
                     temp_list.append(arr[k])
-# print(temp_list)
-print(len(temp_list))
+                now_min = min(temp_list) # min, max 갱신
+                now_max = max(temp_list)
+            # print(temp_list)
+            now_max_len = len(temp_list)
+            total_max_len = max(now_max_len, total_max_len)
+print(total_max_len)
