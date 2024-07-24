c7837919c8804fe17b7602043b0b11b6b678b2c1
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-24 16:09:51 +0900

    Title: 가장 작은 x 찾기, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 240724/가장 작은 x 찾기/find-smallest-x.py
+++ 240724/가장 작은 x 찾기/find-smallest-x.py
@@ -1,11 +1,13 @@
+import sys
+
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-total_min = 1000
+total_min = sys.maxsize
 for i in range(1, arr[0][1] // 2 + 1):
     # print('i', i)
     ok_num = True
-    now_min = 1000
+    now_min = sys.maxsize
     now_num = i
     for j in range(n):
         i = i * 2
@@ -13,7 +15,7 @@ for i in range(1, arr[0][1] // 2 + 1):
         if i < arr[j][0] or i > arr[j][1]:
             ok_num = False
     if ok_num:
-        # print(now_num)
+        # print('now_num', now_num)
         now_min = now_num
     # print(now_min)
     total_min = min(now_min, total_min)
