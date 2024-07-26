3a88a1e3dac7edc81dbf0056fe6589701dab560e
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-26 18:37:34 +0900

    Title: 초기 수열 복원하기, Time: 168ms, Memory: 25MB, Status: Passed - CodeTree

--- 240726/초기 수열 복원하기/restore-initial-sequence.py
+++ 240726/초기 수열 복원하기/restore-initial-sequence.py
@@ -6,18 +6,23 @@ arr = list(map(int, input().split()))
 # ex) 1, 2, 3에 따라 뒤에 값들이 정해짐
 # 리스트에 첫째값 넣어놓고 나머지는 앞에 값에 대한 차이값을 넣으면 될듯
 total_arr = []
+none_arr = []
 for i in range(1, arr[0]):
     now_list = []
     now_list.append(i)
     for j in range(N-1):
+        # print(j)
         # print('now_sum, now_list[j]', arr[j], now_list[j])
         now_val = arr[j] - now_list[j]
-        now_list.append(now_val)
-        # print(now_list)
-    if 0 in now_list:
-        continue
-    total_arr.append(now_list)
-# print(total_arr)
-total_arr.sort(reverse=True)
+        if (now_val <= 0) or (now_val in now_list):
+            break
+        else:
+            now_list.append(now_val)
+        # print('now_list', now_list)
+    if len(now_list) == N:
+        total_arr.append(now_list)
+# print('total_arr', total_arr)
+total_arr.sort()
+# print('total_arr', total_arr)
 for elem in total_arr[0]:
     print(elem, end=" ")
