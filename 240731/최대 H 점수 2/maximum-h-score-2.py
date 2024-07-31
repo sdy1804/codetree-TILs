8e359faf374851203bacfa49c0f9d291460db4d1
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-07-31 18:46:42 +0900

    Title: 최대 H 점수 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240731/최대 H 점수 2/maximum-h-score-2.py
+++ 240731/최대 H 점수 2/maximum-h-score-2.py
@@ -1,6 +1,7 @@
 N, L = map(int, input().split())
 arr = list(map(int, input().split()))
-
+# arr.sort()
+# print(arr)
 # 앞 유형과 마찬가지로 최댓값 설정
 # 최댓값이 0 일 때 -> 1을 L개 더해서 0보다 큰수를 0개 이상 만들 수 있으면 최댓값 만족
 # 최댓값이 1 일 때 -> 1을 L개 더해서 1보다 큰수가 1개 이상 만들 수 있으면 최댓값 만족
@@ -22,6 +23,7 @@ for i in range(max(arr)+1):
     # print(total_max)
     if L > 0:
         max_idx = 0
+        plus = L
         for j in range(len(arr)):
             if arr[j] < i:
                 max_idx = max(j, max_idx)
@@ -29,6 +31,7 @@ for i in range(max(arr)+1):
         for k in range(len(arr)):
             if arr[k] >= i:
                 now_cnt += 1
+        arr[max_idx] -= 1
     if now_cnt >= i:
         total_max = max(total_max, i)
 print(total_max)
