fb4abb3560580b6fd28ca0572a956cc77ed7b815
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-08-01 18:29:36 +0900

    Title: 비둘기와 전기줄, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240801/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 240801/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -7,18 +7,19 @@ for i in range(len(arr)):
     bird_num = arr[i][0]
     bird_line = arr[i][1]
     check_list[bird_num].append(bird_line)
-
+# print(check_list)
 cnt = 0
 for j in range(len(check_list)):
     if check_list[j] == []:
         continue
     if check_list[j].count(1) == check_list[j].count(0):
-        cnt += check_list[j].count(1)
+        for k in range(len(check_list[j])-1):
+            if check_list[j][k] != check_list[j][k+1]:
+                cnt += 1
     elif check_list[j].count(1) != check_list[j].count(0):
         if check_list[j].count(1) == 0 or check_list[j].count(0) == 0:
             continue
-        elif check_list[j].count(1) > check_list[j].count(0):
-            cnt += check_list[j].count(0)
-        elif check_list[j].count(1) < check_list[j].count(0):
-            cnt += check_list[j].count(1)
+        for k in range(len(check_list[j])-1):
+            if check_list[j][k] != check_list[j][k+1]:
+                cnt += 1
 print(cnt)
