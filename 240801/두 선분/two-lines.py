07d8ec6218b98dd59ce1a76452651de90e3e3f72
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-08-01 16:29:29 +0900

    Title: 두 선분, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240801/두 선분/two-lines.py
+++ 240801/두 선분/two-lines.py
@@ -1,6 +1,6 @@
 x1, x2, x3, x4 = map(int, input().split())
 
-if (x2 > x3) or (x4 > x1) or (x1 < x3 and x4 < x2) or (x3 < x1 and x2 < x4):
+if (x1< x3 and x3 < x2 and x2 < x4) or (x3 < x1 and x1 < x4 and x4 < x2) or (x1 < x3 and x4 < x2) or (x3 < x1 and x2 < x4):
     print('intersecting')
 else:
     print('nonintersecting')
