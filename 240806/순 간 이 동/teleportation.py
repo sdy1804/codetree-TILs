8af5df8de963db9764c946ef1d014d001054a69a
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-08-06 17:41:07 +0900

    Title: 순 간 이 동, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240806/순 간 이 동/teleportation.py
@@ -0,0 +1,8 @@
+A, B, x, y = map(int, input().split())
+
+dist = 0
+if abs(A - x) < abs(A - y):
+    dist = abs(A - x) + abs(B - y)
+elif abs(A - y) < abs(A - x):
+    dist = abs(A - y) + abs(B - x)
+print(dist)
