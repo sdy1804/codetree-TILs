b406530fb7c0e32354562e2c0ba26025d86b081e
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-08-06 18:18:12 +0900

    Title: 연속된 숫자 만들기 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240806/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240806/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -4,7 +4,7 @@ arr.sort()
 # case 2 : 두 숫자 간격이 2일 때 -> 2, 4, 9 => 1번 이동 / 2, 4, 6 => 1번 이동
 # case 3 : 모든 숫자 간격이 1 일 때 -> 2, 3, 4 => 0번 이동
 
-if abs(arr[0] - arr[1]) > 2 and abs(arr[1] - arr[2]) > 2:
+if abs(arr[0] - arr[1]) > 2 or abs(arr[1] - arr[2]) > 2:
     print(2)
 elif abs(arr[0] - arr[1]) == 2 or abs(arr[1] - arr[2]) == 2:
     print(1)
