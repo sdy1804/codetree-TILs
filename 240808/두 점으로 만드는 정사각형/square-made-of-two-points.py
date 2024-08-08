a2a3b91367709f537bb604f350564c914dbdae22
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-08-08 18:45:26 +0900

    Title: 두 점으로 만드는 정사각형, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240808/두 점으로 만드는 정사각형/square-made-of-two-points.py
+++ 240808/두 점으로 만드는 정사각형/square-made-of-two-points.py
@@ -3,5 +3,12 @@ a1, b1, a2, b2 = map(int, input().split())
 
 left_down_x = min(x1, a1)
 right_top_x = max(x2, a2)
+left_down_y = min(y1, b1)
+right_top_y = max(y2, b2)
 # print(left_down_x, right_top_x)
-print((right_top_x - left_down_x)**2)
+# print(left_down_y, right_top_y)
+
+if (right_top_x - left_down_x)**2 > (right_top_y - left_down_y)**2:
+    print((right_top_x - left_down_x)**2)
+else:
+    print((right_top_y - left_down_y)**2)
