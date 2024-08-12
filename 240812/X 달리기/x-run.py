73e7bfb9534c79e2e4c0c7352d5caa10a905432f
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-08-12 17:09:23 +0900

    Title: X 달리기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/X 달리기/x-run.py
+++ 240812/X 달리기/x-run.py
@@ -1,26 +1,26 @@
 X = int(input())
 
-curr_pos = 0
+# 속력을 증가시킬지, 유지시킬지, 감소시킬지를 판단
+# 1로 마무리하는 문제 조건을 지키면서 속력을 정할 수 있는 방법?
+# 매번 속력을 증가시켜도 되는지 확인
+
 curr_speed = 1
-sum_time = 0
-while (True):
-    curr_pos += curr_speed
-    sum_time += 1
-    curr_speed += 1
-    increase_sum = 0
+left_dist = X
+curr_time = 0
 
-    if (curr_pos == X):
+while True:
+    left_dist -= curr_speed
+    curr_time += 1
+
+    if left_dist == 0:
         break
-    
-    for i in range(curr_speed):
-        increase_sum += i
-    if X - (curr_pos + curr_speed) < increase_sum:
+    # 증가시켰을 때 이동거리 -> v+1 + v + v-1 + ... + 2 + 1 = (v+1) * (v+2) / 2
+    if left_dist >= (curr_speed + 1) * (curr_speed + 2) / 2:
+        curr_speed += 1
+    # 유지시켰을 때 이동거리 -> v + v-1 + ... + 2 + 1 = v * (v+1) / 2
+    elif left_dist >= (curr_speed + 1) * curr_speed / 2:
+        pass
+    # 이 외에는 속도 감소
+    else:
         curr_speed -= 1
-    
-    stay_sum = 0
-    for i in range(curr_speed):
-        stay_sum += i
-    if X - (curr_pos + curr_speed) < stay_sum:
-        curr_speed -= 1
-
-print(sum_time)
+print(curr_time)
