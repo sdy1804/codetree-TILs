498dd0ab7538c83aef010500dd231cb12aa0c502
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-08-29 18:20:02 +0900

    Title: 병합 정렬 구현, Time: 382ms, Memory: 31MB, Status: Passed - CodeTree

--- 240829/병합 정렬 구현/implement-merge-sort.py
+++ 240829/병합 정렬 구현/implement-merge-sort.py
@@ -1,43 +1,42 @@
 n = int(input())
 arr = list(map(int, input().split()))
+merge_lst = [0] * n
 
-def MergeSort(lst, low, high):
+def MergeSort(low, high):
     if low < high:
         mid = (low + high) // 2
-        MergeSort(lst, low, mid)
-        MergeSort(lst, mid+1, high)
-        Merge(lst, low, mid, high)
+        MergeSort(low, mid)
+        MergeSort(mid+1, high)
+        Merge(low, mid, high)
 
-def Merge(lst, low, mid, high):
+def Merge(low, mid, high):
     i, j = low, mid+1
     idx = low
-    merge_lst = [0 for _ in range(n)]
     
     while i <= mid and j <= high:
-        if lst[i] < lst[j]:
-            merge_lst[idx] = lst[i]
+        if arr[i] < arr[j]:
+            merge_lst[idx] = arr[i]
             idx += 1
             i += 1
         else:
-            merge_lst[idx] = lst[j]
+            merge_lst[idx] = arr[j]
             idx += 1
             j += 1
 
     while i <= mid:
-        merge_lst[idx] = lst[i]
+        merge_lst[idx] = arr[i]
         idx += 1
         i += 1
-    
-    while j <= high:
-        merge_lst[idx] = lst[j]
-        idx += 1   
-        j += 1
-    # print('merge_lst', merge_lst)
-    for index in range(low, high+1):
-        lst[index] = merge_lst[index]
-    # print('lst', lst)
-    return lst
 
-MergeSort(arr, 0, n-1)
+    while j <= high:
+        merge_lst[idx] = arr[j]
+        idx += 1
+        j += 1
+
+    for index in range(low, high+1):
+        arr[index] = merge_lst[index]
+
+
+MergeSort(0, n-1)
 for elem in arr:
     print(elem, end=" ")
