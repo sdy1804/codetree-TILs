ae4d74a98250d52e460063ab5c17d57247377029
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-10-08 22:21:02 +0900

    Title: 떨어지는 1자 블록, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 241008/떨어지는 1자 블록/falling-horizontal-block.py
+++ 241008/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,23 +1,28 @@
 n, m, k = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-# 두번째 행부터 k~k+m-1에 1이 있는지 확인
+# 현재 행에서 다음 행 k~k+m-1에 1이 있는지 또는 끝자락인지 확인
 # 1이 있으면 현재 행의 위치의 k~k-m+1을 1로 변경 후 탈출
-# 1이 없으면 다음번째 행을 대상으로 k~k+m-1에 1이 있는지 확인
+# 1이 없으면 다음번째 행에서 그 다음 행 자리에 k~k+m-1에 1이 있는지 확인
 # 이를 반복
 
 real_k = k-1
 
+def is_end(r):
+    if r+1 == n:
+        return True
+    return False
+
 def is_there_one(r):
     for idx in range(real_k, real_k+m):
-        if grid[r][idx] == 1:
+        if grid[r+1][idx] == 1:
             return True 
     return False
 
-for row in range(1, n):
-    if is_there_one(row):
+for row in range(n):
+    if is_end(row) or is_there_one(row):
         for index in range(real_k, real_k+m):
-            grid[row-1][index] = 1
+            grid[row][index] = 1
         break
 
 for elem in grid:
