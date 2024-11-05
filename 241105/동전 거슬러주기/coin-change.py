64cec25fd8027b83c6cbb816ee6763958aff9935
sdy1804 <82019869+sdy1804@users.noreply.github.com>
2024-11-05 17:00:35 +0900

    Title: 동전 거슬러주기, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 241105/동전 거슬러주기/coin-change.py
+++ 241105/동전 거슬러주기/coin-change.py
@@ -18,6 +18,11 @@ initialize()
 for i in range(1, M+1):
     for j in range(1, N+1):
         if i >= coins[j]:
+            # if dp[i-coins[j]] == INT_MAX:
+            #     continue
             dp[i] = min(dp[i], dp[i-coins[j]]+1)
 
-print(dp[M])
+if dp[M] == INT_MAX:
+    print(-1)
+else:
+    print(dp[M])
